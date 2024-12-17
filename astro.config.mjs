d47d6774c10f198ca22f2cd5bc269eac4e3440f2
Fidel Ernesto <fidehlg89@gmail.com>
2024-12-17 12:13:09 +0000

    feat(astro.config.mjs): added node adapter for standalone mode chore(package.json): updated "@astrojs/node" dependency to "^9.0.0"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import node from '@astrojs/node';
 
 export default defineConfig({
   output: "hybrid",
+  adapter: node({
+    mode: 'standalone', // or 'middleware' depending on your use case
+  }),
   integrations: [tailwind(), react()],
 });
