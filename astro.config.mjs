9643a6c3e1d7caa6e314de6eb589a59d53f2cec3
Fidel Ernesto <fidehlg89@gmail.com>
2025-01-25 14:35:37 +0000

    feat(astro.config.mjs): updated adapter to use node chore(package.json): updated versions of @astrojs/vercel and @astrojs/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
-/* import node from "@astrojs/node"; */
+import node from "@astrojs/node";
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [tailwind(), react()],
   include: ["src", "types"],
 });
