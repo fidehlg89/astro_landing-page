a004e2ed9945c22f35a21ea1d50b2b4b6092aaec
Fidel Ernesto <fidehlg89@gmail.com>
2025-01-25 13:52:11 +0000

    refactor(astro.config.mjs): changed adapter to Vercel and output to 'hybrid'

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
-import node from "@astrojs/node";
+/* import node from "@astrojs/node"; */
 
 export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: "hybrid",
+  adapter: vercel(),
   integrations: [tailwind(), react()],
   include: ["src", "types"],
 });
