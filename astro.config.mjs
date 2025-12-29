ecb497d1a5206d8ccbfde725cd73dfcfb3571833
Fidel Ernesto <fidehlg89@gmail.com>
2025-12-29 00:27:42 +0000

    update code and data

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 import node from "@astrojs/node";
 
 export default defineConfig({
@@ -9,7 +10,5 @@ export default defineConfig({
     mode: 'standalone',
   }),
   integrations: [tailwind(), react()],
-  include: ["src", "types"],
-  site: 'https://fidehlg89.github.io',
-  base: 'astro_landing-page',
+  include: ["src", "types"]
 });
